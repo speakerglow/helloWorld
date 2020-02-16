9a0f46d40f904ddc41f08de92c2d8c333ef9887b
Artem <speaker_glow_2018@mail.ru>
2020-02-16 21:08:20 +0500

    Create new repository

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = """
+            BuildType=${BuildConfig.BUILD_TYPE}
+            VersionCode=${BuildConfig.VERSION_CODE}
+            VersionName=${BuildConfig.VERSION_NAME}
+            ApplicationId=${BuildConfig.APPLICATION_ID}
+        """.trimIndent()
+    }
+}
